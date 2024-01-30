0892f1b031fd091e8eb604caa18223abf2526706
cgregoryCH <84070107+cgregoryCH@users.noreply.github.com>
2024-01-30 16:50:24 +0000

    DSND-2170: Add ERIC container config to allow ERIC proxy (#13)

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.235"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.243"
 
 
   # Environmental configuration
@@ -71,10 +71,19 @@ module "ecs-service" {
   task_secrets                = local.task_secrets
   app_environment_filename    = local.app_environment_filename
   use_set_environment_files   = local.use_set_environment_files
+
+  # eric options for eric running API module
+  use_eric_reverse_proxy    = true
+  eric_version              = var.eric_version
+  eric_cpus                 = var.eric_cpus
+  eric_memory               = var.eric_memory
+  eric_port                 = local.eric_port
+  eric_environment_filename = local.eric_environment_filename
+  eric_secrets              = local.eric_secrets
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.235"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.243"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
