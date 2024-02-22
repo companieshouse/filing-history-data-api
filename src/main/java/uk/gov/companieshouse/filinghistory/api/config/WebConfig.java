39fa825cecb3a068c492a60b1c63057a9ffd5e5f
cgregoryCH <cgregory@companieshouse.gov.uk>
2024-02-22 12:40:32 +0000

    DSND-2280: Fix request logging on runtime exceptions
    * Replace request logging interceptor with filter.

--- src/main/java/uk/gov/companieshouse/filinghistory/api/config/WebConfig.java
+++ src/main/java/uk/gov/companieshouse/filinghistory/api/config/WebConfig.java
@@ -4,14 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import uk.gov.companieshouse.filinghistory.api.auth.AuthenticationInterceptor;
-import uk.gov.companieshouse.filinghistory.api.logging.RequestLoggingInterceptor;
 
 @Configuration
 class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new RequestLoggingInterceptor());
         registry.addInterceptor(new AuthenticationInterceptor())
                 .addPathPatterns("/**")
                 .excludePathPatterns("/healthcheck");
