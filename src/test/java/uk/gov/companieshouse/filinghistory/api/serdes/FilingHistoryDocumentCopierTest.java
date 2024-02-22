7397405baa43024b9f42ffaa26caf692e5f069d9
sthompsonCH <sthompson@companieshouse.gov.uk>
2024-02-22 12:02:27 +0000

    DSND-2280: Add unit tests for internal server error exception

--- src/test/java/uk/gov/companieshouse/filinghistory/api/serdes/FilingHistoryDocumentCopierTest.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/api/serdes/FilingHistoryDocumentCopierTest.java
@@ -3,17 +3,21 @@ package uk.gov.companieshouse.filinghistory.api.serdes;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.filinghistory.api.exception.InternalServerErrorException;
 import uk.gov.companieshouse.filinghistory.api.model.FilingHistoryDocument;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,5 +47,30 @@ class FilingHistoryDocumentCopierTest {
         assertNotNull(copyDocument);
         assertEquals(copyDocument, copyInputDocument);
         assertNotEquals(copyDocument, inputDocument);
+    }
+
+    @Test
+    void shouldThrowInternalServerErrorWhenJsonProcessingExceptionThrownOnWrite() throws Exception {
+        // given
+        when(objectMapper.writeValueAsString(any())).thenThrow(JsonProcessingException.class);
+
+        // when
+        Executable executable = () -> copier.deepCopy(inputDocument);
+
+        // then
+        assertThrows(InternalServerErrorException.class, executable);
+    }
+
+    @Test
+    void shouldThrowInternalServerErrorWhenJsonProcessingExceptionThrownOnRead() throws Exception {
+        // given
+        when(objectMapper.writeValueAsString(any())).thenReturn("object_as_string");
+        when(objectMapper.readValue(anyString(), eq(FilingHistoryDocument.class))).thenThrow(JsonProcessingException.class);
+
+        // when
+        Executable executable = () -> copier.deepCopy(inputDocument);
+
+        // then
+        assertThrows(InternalServerErrorException.class, executable);
     }
 }
