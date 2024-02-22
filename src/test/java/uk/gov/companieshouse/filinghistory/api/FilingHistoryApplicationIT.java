5f545ac00898746a61e95458db02087f481fcc49
cgregoryCH <84070107+cgregoryCH@users.noreply.github.com>
2024-02-22 13:28:01 +0000

    DSND-2280: Fix request logging on runtime exceptions (#22)
    * Replace request logging interceptor with filter.

--- src/test/java/uk/gov/companieshouse/filinghistory/api/FilingHistoryApplicationIT.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/api/FilingHistoryApplicationIT.java
@@ -11,10 +11,11 @@ import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class FilingHistoryApplicationIT {
 
     @Autowired
