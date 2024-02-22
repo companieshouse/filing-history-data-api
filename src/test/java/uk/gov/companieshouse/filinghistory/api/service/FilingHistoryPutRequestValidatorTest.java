23eb2576bc96bb4fc8c6449e9b8ca253d01fe5db
sthompsonCH <sthompson@companieshouse.gov.uk>
2024-02-22 12:27:00 +0000

    DSND-2280: Remove unused imports

--- src/test/java/uk/gov/companieshouse/filinghistory/api/service/FilingHistoryPutRequestValidatorTest.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/api/service/FilingHistoryPutRequestValidatorTest.java
@@ -1,6 +1,5 @@
 package uk.gov.companieshouse.filinghistory.api.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
