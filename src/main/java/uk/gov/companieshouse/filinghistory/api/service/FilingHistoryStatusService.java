7185b1a14753d75564f8f9feb4f8e5bae5681cc1
sthompsonCH <sthompson@companieshouse.gov.uk>
2024-03-20 14:31:47 +0000

    DSND-2420: Refactor processStatus to reduce complexity

--- src/main/java/uk/gov/companieshouse/filinghistory/api/service/FilingHistoryStatusService.java
+++ src/main/java/uk/gov/companieshouse/filinghistory/api/service/FilingHistoryStatusService.java
@@ -20,41 +20,44 @@ public class FilingHistoryStatusService implements StatusService {
         Pattern pattern = Pattern.compile("^([A-Z]{2}|R0|)(\\d+)");
         Matcher matcher = pattern.matcher(companyNumber);
 
-        final String prefix = matcher.find() ? matcher.group(1) : null;
-        final int number = Integer.parseInt(matcher.group(2));
+        final String companyNumberPrefix = matcher.find() ? matcher.group(1) : null;
+        final int extractedNumber = Integer.parseInt(matcher.group(2));
 
-        return Optional.ofNullable(prefix)
-                .map(p -> {
-                    if (p.isEmpty()) {
+        return Optional.ofNullable(companyNumberPrefix)
+                .map(prefix -> {
+                    if (prefix.isEmpty()) {
                         // If prefix == ""
-                        return getStatusRuleProperties("NORMAL").status();
+                        return getPrefixProperties("NORMAL").status();
                     }
-                    // If prefix is not null nor empty
-                    return Optional.ofNullable(getStatusRuleProperties(p))
-                            // If a prefix property exists for prefix
-                            .map(prefixProperties -> {
-                                // If prefixProperties contains a 'from' array
-                                if (prefixProperties.from() != null) {
-                                    for (FromProperties fromProperties : prefixProperties.from()) {
-                                        if (fromProperties.number() > number) {
-                                            return fromProperties.status();
-                                        }
-                                    }
-                                }
-                                // If prefixProperties does not contain a from array
-                                return prefixProperties.status();
-                            })
-                            // If a prefix property does not exist for prefix
-                            .orElse(getStatusRuleProperties("UNKNOWN_PREFIX").status());
+                    return getStatusFromValidPrefix(prefix, extractedNumber);
                 })
-                // If prefix is null
-                .orElse(getStatusRuleProperties("INVALID_FORMAT").status());
+                .orElse(getPrefixProperties("INVALID_FORMAT").status());
     }
 
-    private PrefixProperties getStatusRuleProperties(final String prefix) {
+    private PrefixProperties getPrefixProperties(final String prefix) {
         return statusRuleProperties
                 .filingHistory()
                 .getOrDefault(prefix, null);
+    }
+
+    private String getStatusFromValidPrefix(final String validPrefix, final int number) {
+        // If prefix is not null nor empty
+        return Optional.ofNullable(getPrefixProperties(validPrefix))
+                // If a prefix property exists for prefix
+                .map(prefixProperties -> {
+                    // If prefixProperties contains a 'from' array
+                    if (prefixProperties.from() != null) {
+                        for (FromProperties fromProperties : prefixProperties.from()) {
+                            if (fromProperties.number() > number) {
+                                return fromProperties.status();
+                            }
+                        }
+                    }
+                    // If prefixProperties does not contain a 'from' array
+                    return prefixProperties.status();
+                })
+                // If a prefix property does not exist for prefix
+                .orElse(getPrefixProperties("UNKNOWN_PREFIX").status());
     }
 }
 
