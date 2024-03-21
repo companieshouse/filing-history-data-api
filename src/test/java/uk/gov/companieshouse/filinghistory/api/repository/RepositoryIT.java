d4a2280313d2813dd3470421415df55ac78d7011
sthompsonCH <sthompson@companieshouse.gov.uk>
2024-03-21 14:32:26 +0000

    DSND-2420: Add dynamic property source to itest
    * RepostitoryIT.java was not working when running all tests in the
      package. Adding the dynamic property source seems to fix this.

--- src/test/java/uk/gov/companieshouse/filinghistory/api/repository/RepositoryIT.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/api/repository/RepositoryIT.java
@@ -16,6 +16,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -37,6 +39,11 @@ class RepositoryIT {
 
     @Container
     private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:5.0.12");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Autowired
     private MongoTemplate mongoTemplate;
