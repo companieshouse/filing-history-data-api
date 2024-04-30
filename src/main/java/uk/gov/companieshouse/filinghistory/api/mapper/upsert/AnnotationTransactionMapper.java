2fd95a101bb9fbff6eb16c6bd4b8581412a1aa1e
sthompsonCH <sthompson@companieshouse.gov.uk>
2024-04-30 15:03:56 +0100

    DSND-2369: WIP - begin refactor for annotation transaction mapper

--- src/main/java/uk/gov/companieshouse/filinghistory/api/mapper/upsert/AnnotationTransactionMapper.java
+++ src/main/java/uk/gov/companieshouse/filinghistory/api/mapper/upsert/AnnotationTransactionMapper.java
@@ -1,15 +1,11 @@
 package uk.gov.companieshouse.filinghistory.api.mapper.upsert;
 
 import java.time.Instant;
-import java.util.List;
-import java.util.Optional;
 import java.util.function.Supplier;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.filinghistory.InternalData;
 import uk.gov.companieshouse.api.filinghistory.InternalFilingHistoryApi;
-import uk.gov.companieshouse.filinghistory.api.exception.ConflictException;
-import uk.gov.companieshouse.filinghistory.api.logging.DataMapHolder;
 import uk.gov.companieshouse.filinghistory.api.model.mongo.FilingHistoryAnnotation;
 import uk.gov.companieshouse.filinghistory.api.model.mongo.FilingHistoryData;
 import uk.gov.companieshouse.filinghistory.api.model.mongo.FilingHistoryDocument;
@@ -18,16 +14,15 @@ import uk.gov.companieshouse.filinghistory.api.model.mongo.FilingHistoryDocument
 public class AnnotationTransactionMapper extends AbstractTransactionMapper {
 
     private final DataMapper dataMapper;
-    private final ChildMapper annotationChildMapper;
+    private final ChildListMapper<FilingHistoryAnnotation> childListMapper;
     private final Supplier<Instant> instantSupplier;
 
     public AnnotationTransactionMapper(LinksMapper linksMapper,
-            DataMapper dataMapper,
-            ChildMapper annotationChildMapper,
-            Supplier<Instant> instantSupplier) {
+                                       DataMapper dataMapper, ChildListMapper<FilingHistoryAnnotation> childListMapper,
+                                       Supplier<Instant> instantSupplier) {
         super(linksMapper);
         this.dataMapper = dataMapper;
-        this.annotationChildMapper = annotationChildMapper;
+        this.childListMapper = childListMapper;
         this.instantSupplier = instantSupplier;
     }
 
@@ -36,54 +31,24 @@ public class AnnotationTransactionMapper extends AbstractTransactionMapper {
         if (StringUtils.isBlank(request.getInternalData().getParentEntityId())) {
             data = dataMapper.map(request.getExternalData(), data);
         }
-        return data.annotations(List.of(annotationChildMapper.mapChild(request, new FilingHistoryAnnotation())));
+        childListMapper.mapChildList(request, data.getAnnotations(), data::annotations);
+        return data;
     }
 
     @Override
     public FilingHistoryDocument mapFilingHistoryToExistingDocumentUnlessStale(InternalFilingHistoryApi request,
-            FilingHistoryDocument existingDocument) {
-        final String requestEntityId = request.getInternalData().getEntityId();
+                                                                               FilingHistoryDocument existingDocument) {
+        childListMapper.mapChildList(
+                request,
+                existingDocument.getData().getAnnotations(),
+                existingDocument.getData()::annotations);
 
-        Optional.ofNullable(existingDocument.getData().getAnnotations())
-                .ifPresentOrElse(
-                        annotationList -> annotationList.stream()
-                                .filter(annotation -> requestEntityId.equals(annotation.getEntityId()))
-                                .findFirst()
-                                .ifPresentOrElse(annotation -> {
-                                            if (isDeltaStale(request.getInternalData().getDeltaAt(),
-                                                    annotation.getDeltaAt())) {
-                                                LOGGER.error(STALE_DELTA_ERROR_MESSAGE.formatted(
-                                                                request.getInternalData().getDeltaAt(),
-                                                                annotation.getDeltaAt()),
-                                                        DataMapHolder.getLogMap());
-                                                throw new ConflictException("Stale delta when updating annotation");
-                                            }
-                                            // Update already existing annotation from list
-                                            annotationChildMapper.mapChild(request, annotation);
-                                        },
-                                        // Add new annotation to existing annotations list
-                                        () -> {
-                                            if (annotationList.stream()
-                                                    .anyMatch(annotation ->
-                                                            StringUtils.isBlank(annotation.getEntityId()))) {
-                                                LOGGER.info(
-                                                        MISSING_ENTITY_ID_ERROR_MSG.formatted(requestEntityId),
-                                                        DataMapHolder.getLogMap()
-                                                );
-                                            }
-                                            annotationList
-                                                    .add(annotationChildMapper
-                                                            .mapChild(request, new FilingHistoryAnnotation()));
-                                        }),
-                        // Add new annotation to a new annotations list
-                        () -> mapFilingHistoryData(request, existingDocument.getData())
-                );
         return mapTopLevelFields(request, existingDocument);
     }
 
     @Override
     protected FilingHistoryDocument mapTopLevelFields(InternalFilingHistoryApi request,
-            FilingHistoryDocument document) {
+                                                      FilingHistoryDocument document) {
         document.getData().paperFiled(request.getExternalData().getPaperFiled());
 
         final InternalData internalData = request.getInternalData();
@@ -97,8 +62,7 @@ public class AnnotationTransactionMapper extends AbstractTransactionMapper {
                     .matchedDefault(internalData.getMatchedDefault())
                     .originalDescription(internalData.getOriginalDescription());
         } else {
-            document
-                    .entityId(internalData.getParentEntityId());
+            document.entityId(internalData.getParentEntityId());
         }
         return document
                 .companyNumber(internalData.getCompanyNumber())
