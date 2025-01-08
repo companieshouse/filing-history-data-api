20b57a6a2dd3260c46e54c1b8d4ca0e628815b93
johncookch <jcook@companieshouse.gov.uk>
2025-01-08 18:47:57 +0000

    CC-1685: Vanilla startup

--- src/test/java/uk/gov/companieshouse/filinghistory/api/repository/RepositoryIT.java
+++ src/test/java/uk/gov/companieshouse/filinghistory/api/repository/RepositoryIT.java
@@ -55,12 +55,12 @@ class RepositoryIT {
     private static final int TOTAL_RESULTS_NUMBER = 55;
 
     @Container
-    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.8")
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.8");
 //            .withCommand("--replSet rs0 --bind_ip_all")
-            .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(RepositoryIT.class)))
-            .waitingFor(new LogMessageWaitStrategy()
-                .withRegEx("(?i).*mongod startup complete.*")
-                .withStartupTimeout(Duration.ofSeconds(60)));
+//            .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(RepositoryIT.class)))
+//            .waitingFor(new LogMessageWaitStrategy()
+//                .withRegEx("(?i).*mongod startup complete.*")
+//                .withStartupTimeout(Duration.ofSeconds(60)));
 
     @Autowired
     private MongoTemplate mongoTemplate;
