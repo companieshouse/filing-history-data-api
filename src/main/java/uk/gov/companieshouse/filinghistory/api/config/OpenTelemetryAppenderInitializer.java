18eeab135e45b8b4bf6dffd1c881dc8cc3b4236b
markpit <11330756+markpit@users.noreply.github.com>
2026-08-18 11:48:17 +0100

    ASM-2678 Add opentelemetry config

--- src/main/java/uk/gov/companieshouse/filinghistory/api/config/OpenTelemetryAppenderInitializer.java
+++ src/main/java/uk/gov/companieshouse/filinghistory/api/config/OpenTelemetryAppenderInitializer.java
@@ -3,9 +3,11 @@ package uk.gov.companieshouse.filinghistory.api.config;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(prefix = "management.opentelemetry", name = "enabled", havingValue = "true")
 class OpenTelemetryAppenderInitializer implements InitializingBean {
 
     private final OpenTelemetry openTelemetry;
